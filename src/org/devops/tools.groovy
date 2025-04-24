030e0446acdfb6a4879525bc1834e86a56211f9d
molido <1092337618@qq.com>
2025-04-24 15:24:28 +0800

    Update tools.groovy
    打印颜色

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -2,3 +2,12 @@ package org.devops
 def PrintMes(content){
   println(content)
 }
+
+//格式化输出
+def PrintMes(value, color){
+  colors = ['red' : "\033[40;31m >>>>>>>>>>>${value} <<<<<<<<<<<< \033[0m", 
+           'blue' : "\033[47;34m ${value} \033[0m"]
+  ansiColor('xterm'){
+    printlin(colors[color])
+  }
+}
